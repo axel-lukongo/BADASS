3187fc705a07075c3366b6b3df26eedce13acd39
axel lukongo <axel_lukongo@git.com>
2025-01-06 18:45:19 +0100

    feat(part 2): i finished part 2 and i add a config for the host

--- p2/config_host.sh
+++ p2/config_host.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-host_id=$(hostname | cut -d '-' -f 2)
+host_id=$(hostname)
 
-if [ "$host_id" -eq 1 ]; then
+if [ "$host_id" == "host_alukongo-1" ]; then
 	ip addr add 192.168.1.3/24 dev eth0
 	ip link set eth0 up
 	ip route add default via 192.168.1.1
--- p2/copie_script_in_router.sh
+++ p2/copie_script_in_router.sh
@@ -11,3 +11,13 @@ for cont_id in $(docker ps -q); do
 		docker exec $cont_id bash /config_VXLAN.sh $1
 	fi
 done
+
+
+###### copie and exec the config_host #####
+#for cont_id in $(docker ps -q); do
+#        if [[ $(docker exec $cont_id hostname | cut -d '_' -f 1) == 'host' ]]; then
+#                docker cp ./config_host.sh $cont_id:/ && \
+#                docker exec $cont_id bash /config_host.sh
+#        fi
+#done
+
