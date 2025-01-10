58936a9d89db95728c391b16e9c4f83f7869b405
axel lukongo <axel_lukongo@git.com>
2025-01-10 22:16:40 +0100

    feat: i add configfor part 3 but it doesn't work

--- p3/router_vxlan_conf.sh
+++ p3/router_vxlan_conf.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ip link add name vxlan10 type vxlan id 10 dstport 4789
+ip link set dev br0 up
+ip link set dev vxlan10 up
 
 brctl addbr br0
 brctl addif br0 eth1
 brctl addif br0 vxlan10
 
-ip link set dev br0 up
-ip link set dev vxlan10 up
+
+
